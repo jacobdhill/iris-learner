128d23611b0ad0aafd3e897e520cdc40c6460294
= <jacob.d.hill@outlook.com>
2022-02-05 16:30:37 -0600

    added POST / + vars for backend

--- frontend/index.js
+++ frontend/index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const p = 80;
+  const c = "iris setosa";
+  const d =
+    "Variable in stature, Iris setosa (Bristle-Pointed Iris) is a rhizomatous perennial forming a clump of stiff, narrow, sword-like, mid-green leaves, 1-2 ft. long (30-60 cm), with a prominent midrib and a purplish tinged base. The foliage arises from shallowly rooted, large, branching rhizomes forming clumps.";
+
+  res.render("index", { percentage: p, classification: c, description: d });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen(80);
